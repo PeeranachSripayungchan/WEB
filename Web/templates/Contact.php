21c0b9df09e204b49377eca07ed70f72514463f4
Poonpipatpor <71426366+Poonpipatpor@users.noreply.github.com>
2022-03-16 13:42:11 +0700

    Add files via upload
    Add Contact File

---
+++ Web/templates/Contact.php
@@ -0,0 +1,34 @@
+<html>
+<div style="background-color:#ECF0F1;margin-left: 20%;border-radius: 6px;height: 850px;width: 750px">
+<h1><p style="color:#0DA9BA" align="center">Contact</p></h1>
+<body bgcolor = "D3D3D3">
+<img src="Map.jpg" width="750" height="550">
+<h2><p align="center" style="color:#0DA9BA">Telephone Number: 080-000-0000</p></h2>
+<h2><p align="center" style="color:#0DA9BA">Address: 99/999 ถ.เอกชัย เขตสยาม แขวงสยาม กรุงเทพฯ </p></h2>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.btn {
+  background-color: DodgerBlue;
+  border: none;
+  color: white;
+  padding: 12px 16px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+/* Darker background on mouse-over */
+.btn:hover {
+  background-color: RoyalBlue;
+}
+</style>
+</head>
+
+
+<body>
+<button class="btn" style="margin-left:70%;border-radius: 6px"><i class="fa fa-arrow-left"></i> Previous</button>
+</body>
+</html>
